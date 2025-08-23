4e8bdbfd94709a007f6620343b432865510e6184
myueqf <myueqf@gmail.com>
2025-08-23 19:46:51 +0800

    调整默认预设XwX

--- shaders/settings.glsl
+++ shaders/settings.glsl
@@ -21,7 +21,7 @@ float knee = 0.5; // 软阈值范围
 float bloomScale = 0.03; // 发光强度缩放
 float noiseScale1 = 0.005; // 近距离噪声幅度
 float noiseScale2 = 0.012; // 远距离噪声幅度
-#define EXPONENT 1.5 // [0.5 1.0 1.5 2.0 2.5] 强度
+#define EXPONENT 1.0 // [0.5 1.0 1.5 2.0 2.5] 强度
 float exponent = EXPONENT;
 
 /* 昼夜亮度增益 */
